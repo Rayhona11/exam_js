e817b8781e6e33bdc416c811b2ea426f2bf32f43
Rayhona <rayhonamirzoyeva.ru>
2024-03-20 11:45:16 +0500

    add javascript

---
+++ scripts/events.js
@@ -0,0 +1,10 @@
+// EVENTS
+let inp = document.querySelector('input')
+let h1 = document.querySelector('h1')
+let menu = document.querySelector('.menu')
+let icon = document.querySelector('.menu1')
+
+icon.addEventListener('click',()=>{
+   menu.style.display = "block"
+   menu.style.padding = "7%"    
+})

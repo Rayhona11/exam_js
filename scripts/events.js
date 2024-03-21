b20ce2d9c7cf4a3d278cb714e2e1aa3e8d25dae1
Rayhona <rayhonamirzoyeva.ru>
2024-03-21 21:25:53 +0500

    ngjrh

--- scripts/events.js
+++ scripts/events.js
@@ -5,10 +5,15 @@ let menu = document.querySelector('.menu')
 let btn = document.querySelector('.btn')
 let back = document.querySelector('.comeback')
 
+
+// When we opened the Dashboard 
 btn.addEventListener('click',()=>{
    menu.style.display = "block"
 })
+// When we close the Dashboard
 back.addEventListener('click',()=>{
    menu.style.display = "none"
 })
+
+
 
